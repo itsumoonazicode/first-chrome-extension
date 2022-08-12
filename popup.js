f5eaaafebd767e01f4682460aa3182077a984bd6
itsumoonazicode <dyingkouki@gmail.com>
2022-08-12 18:09:12 +0900

    ウェブページ上で動かす用のスクリプトを作成

---
+++ popup.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", async() => {
+	let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+	chrome.scripting.executeScript({
+		target: {
+			tabId: tab.id
+		},
+		function: onRun
+	});
+});
+
+function onRun() {
+	document.body.style.backgroundColor = "#fcc";
+}
